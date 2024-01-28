b1457dc647f469592adbaefd0975d89439da7b69
sietebonduy <den768653@gmail.com>
2024-01-28 23:09:52 +0300

    prettier listings for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.delete_all
+
+Product.create!(title: 'Design and Build Great Web APIs',
+                description: %{<p>
+<em>Robust, Reliable, and Resilient</em>
+APIs are transforming the business world at an increasing pace.
+Gain the essential skills needed to quickly design, build,
+and deploy quality web APIs that are robust, reliable, and resilient.
+Go from initial design through prototyping and implementation to deployment
+of mission-critical APIs for your organization. Test, secure, and deploy
+your API with confidence and avoid the “release into production” panic.
+Tackle just about any API challenge with more than a dozen open-source
+utilities and common programming patterns you can apply right away.
+</p>},
+                image_url: 'maapis.png',
+                price: 24.95)
+
+Product.create!(title: 'Docker for Rails Developers',
+                description: %{<p> <em>...</em> ...</p>},
+                image_url: 'ridocker.png',
+                price: 18.95)
